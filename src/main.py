6b9000ec461bcafe6f9fa8a37c92ccfe1ed78018
Laande <laande.pro@gmail.com>
2024-05-16 00:39:21 +0200

    add typing & test fix diff

--- src/main.py
+++ src/main.py
@@ -1,54 +1,54 @@
 import sys
-from github import Github
 import os
+from github import Github
 
+# Typing stuff
+from github.Repository import Repository
+from github.Commit import Commit
+from typing import Optional, Dict, List
+
+# Utils stuff
 from utils import status
 
 
-def get_latest_commit(repo):
+def get_latest_commit(repo: Repository) -> Commit:
     return repo.get_commits()[0]
 
-
-def get_previous_commit(commit):
+def get_previous_commit(commit: Commit) -> Optional[Commit]:
     return commit.parents[0] if commit.parents else None
 
-
-def get_previous_files_dict(previous_commit):
+def get_previous_files_dict(previous_commit: Optional[Commit]) -> Dict[str, any]:
     if previous_commit:
         return {file.filename: file for file in previous_commit.files}
     else:
-        return {}
-
+        return {}   
 
 @status.print_status("comparing")
-def comment_on_diffs(repo, latest_commit, previous_commit, folder=None):
-    # sourcery skip: use-named-expression
+def comment_on_diffs(repo: Repository, latest_commit: Commit, previous_commit: Optional[Commit], folder: Optional[str] = None) -> None:
     files = latest_commit.files
     for file in files:
-        if not folder or file.filename.startswith(folder):
+        if not folder or file.filename.startswith(folder):            
             diff = repo.compare(previous_commit.sha, latest_commit.sha)
-            filtered_diff_lines = [line for line in diff.split('\n') if line.startswith('+') or line.startswith('-')]
+            filtered_diff_lines = [line for line in diff.diff_url.split('\n') if line.startswith('+') or line.startswith('-')]
             if filtered_diff_lines:
                 comment_text = f"Modifications in {file.filename}:\n"
                 comment_text += '\n'.join(filtered_diff_lines)
                 latest_commit.create_comment(body=comment_text)
                 print(f"Commented diff for {file.filename}")
 
+def main() -> None:
+    github_token: str = os.environ['GITHUB_TOKEN']
+    repository_name: str = os.environ['GITHUB_REPOSITORY']
 
-def main():
-    github_token = os.environ['GITHUB_TOKEN']
-    repository_name = os.environ['GITHUB_REPOSITORY']
+    g: Github = Github(github_token)
+    repo: Repository = g.get_repo(repository_name)
 
-    g = Github(github_token)
-    repo = g.get_repo(repository_name)
+    latest_commit: Commit = get_latest_commit(repo)
+    previous_commit: Optional[Commit] = get_previous_commit(latest_commit)
 
-    latest_commit = get_latest_commit(repo)
-    previous_commit = get_previous_commit(latest_commit)
-
-    folder = sys.argv[1] if len(sys.argv) > 1 else None
+    folder: Optional[str] = sys.argv[1] if len(sys.argv) > 1 else None
 
     comment_on_diffs(repo, latest_commit, previous_commit, folder)
-
 
 if __name__ == "__main__":
     main()
